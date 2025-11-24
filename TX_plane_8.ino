e5bc0c9eda3630f5e866b5d06ee0174281fcb814
jarek <iaroslav.pavlov@unibas.ch>
2025-11-24 16:09:10 +0100

    using 115th channel

--- TX_plane_8.ino
+++ TX_plane_8.ino
@@ -42,7 +42,7 @@ ResponseSignal responseData;
                                        //Configure the NRF24 module
     radio.begin();
     radio.openWritingPipe(pipe);
-    radio.setChannel(100);
+    radio.setChannel(115);
     radio.setAutoAck(false);
     radio.setDataRate(RF24_250KBPS);    // The lowest data rate value for more stable communication
     radio.setPALevel(RF24_PA_MAX);      // Output power is set for maximum
